31e9c5b34fac8c495a68ba3b8e3edf38b321e1a8
Oleh Dobysh <odobish@gmail.com>
2019-06-20 17:17:27 +0300

    Add project files.

---
+++ RoadMapSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RoadMapSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
