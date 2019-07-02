3df67023c2d91d71a8b0fdc3fdfd9b6a091f1cbd
Oleksii Roik <oleksii.roik.pz.2016@lpnu.ua>
2019-07-02 20:14:00 +0300

    Depiction fixes

--- RoadMapSystem/ViewModels/LoginModel.cs
+++ RoadMapSystem/ViewModels/LoginModel.cs
@@ -9,10 +9,14 @@ namespace RoadMapSystem.ViewModels
     public class LoginModel
     {
         [Required(ErrorMessage = "Не вказаний Логін працівника")]
+
+        [Display(Name = "Логін")]
         public string Login { get; set; }
 
         [Required(ErrorMessage = "Не вказаний Пароль працівника")]
         [DataType(DataType.Password)]
+
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
     }
 }
--- RoadMapSystem/ViewModels/RegisterModel.cs
+++ RoadMapSystem/ViewModels/RegisterModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,20 +8,31 @@ namespace RoadMapSystem.ViewModels
 {
     public class RegisterModel
     {
+        [Display(Name = "Логін")]
         public string Login { get; set; }
-
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
 
+        [Display(Name = "Електронна адреса")]
         public string Email { get; set; }
 
+        [Display(Name = "Імя")]
         public string Name { get; set; }
 
+        [Display(Name = "Прізвище")]
         public string Surname { get; set; }
 
+        [Display(Name = "По-батькові")]
         public string Patronymic { get; set; }
 
+        [Display(Name = "Номер телефону")]
         public string PhoneNumber { get; set; }
+
+        [Display(Name = "Посада")]
         public int EmployeeRoleId { get; set; }
+
+
+        [Display(Name = "Технічний рівень")]
         public int RankId { get; set; }
 
         public int? MentorId { get; set; }
